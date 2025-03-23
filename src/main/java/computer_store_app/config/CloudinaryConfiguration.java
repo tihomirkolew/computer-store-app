4c827d6f8eef6fc14460eb2e0bdcb8b474130913
Tihomir Kolev <sineadfly@gmail.com>
2025-03-24 00:32:24 +0200

    cloudinary secret removed, it is not used

--- src/main/java/computer_store_app/config/CloudinaryConfiguration.java
+++ src/main/java/computer_store_app/config/CloudinaryConfiguration.java
@@ -12,9 +12,9 @@ public class CloudinaryConfiguration {
     @Bean
     public Cloudinary cloudinary() {
         final Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", "dwgcb1ecd");
-        config.put("api_key", "573971848884194");
-        config.put("api_secret", "WyP8ZA9RR91yxx3qy71zNAsRwR0");
+        config.put("cloud_name", "");
+        config.put("api_key", "");
+        config.put("api_secret", "");
         return new Cloudinary(config);
     }
 }
